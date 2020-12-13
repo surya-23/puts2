ea3eca2ff267329e824dc7ed5848b820634785ed
surya-23 <55230183+surya-23@users.noreply.github.com>
2020-12-13 16:45:14 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -2,18 +2,51 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
 
+@app.route('/average', methods=['GET'])
+def average():
+    bar = request.args.to_dict()
+    print(bar)
+    s = bar['X']
+    p = s.split(",")
+    sum = 0
+    for x in p :
+        if x.find('.') == -1 :
+            sum = sum + int(x)
+        else :
+            sum = sum +float(x)
+    d = sum/len(p)
+    return str(d)
 
-@app.route('/add')
-def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
+@app.route('/mean', methods=['GET'])
+def mean():
+    bar = request.args.to_dict()
+    print(bar)
+    s = bar['X']
+    p = s.split(",")
+    sum = 0
+    for x in p :
+        if x.find('.') == -1 :
+            sum = sum + int(x)
+        else :
+            sum = sum +float(x)
+    d = sum/len(p)
+    return str(d)
 
+@app.route('/avg', methods=['GET'])
+def avg():
+    bar = request.args.to_dict()
+    print(bar)
+    s = bar['X']
+    p = s.split(",")
+    sum = 0
+    for x in p :
+        if x.find('.') == -1 :
+            sum = sum + int(x)
+        else :
+            sum = sum +float(x)
+    d = sum/len(p)
+    return str(d)
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(port = 80 ,debug=True)
